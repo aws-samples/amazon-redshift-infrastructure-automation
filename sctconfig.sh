e6c962200d875befbb5162fe42b2a4fc25b113d0
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2021-08-31 09:28:41 -0400

    Update sctconfig.sh

--- sctconfig.sh
+++ sctconfig.sh
@@ -6,7 +6,7 @@ mkdir SCT
 cd SCT
 curl -o aws-schema-conversion-tool-1.0.latest.zip  https://s3.amazonaws.com/publicsctdownload/Windows/aws-schema-conversion-tool-1.0.latest.zip
 tar -xf aws-schema-conversion-tool-1.0.latest.zip
-for %I in ("C:\SCT\AWS Schema*.msi") do %SystemRoot%\System32\msiexec.exe /i "%I" /qn
+for %I in ("C:\SCT\AWS Schema*.msi") do msiexec.exe /i "%I" /qn
 curl -o AWSCLIV2.msi https://awscli.amazonaws.com/AWSCLIV2.msi
 msiexec.exe /i "AWSCLIV2.msi" /passive /l logcli.txt
 set PATH="c:\Program Files\Amazon\AWSCLIV2\";%PATH%
