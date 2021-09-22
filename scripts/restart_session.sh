6b3b5266431dde0161e6a4ad3152854bb500d248
Paul Villena <pvillena@amazon.com>
2021-09-23 09:57:49 +1000

    Updated restart_session.sh.sh

--- scripts/restart_session.sh
+++ scripts/restart_session.sh
@@ -2,3 +2,6 @@ sudo yum -y install gcc gcc-c++ python3 python3-devel unixODBC unixODBC-devel aw
 sudo npm install -g aws-cdk
 source ~/amazon-redshift-infrastructure-automation/.env/bin/activate
 cd ~/amazon-redshift-infrastructure-automation/
+python3 -m venv .env
+source .env/bin/activate
+pip install -r requirements.txt
