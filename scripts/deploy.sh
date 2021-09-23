04f8d411090652e39fcbb6bc66049700ddbec670
Paul Villena <pvillena@amazon.com>
2021-09-23 10:02:30 +1000

    Updated deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,7 @@ pip install -r requirements.txt
 LINE='~/amazon-redshift-infrastructure-automation/scripts/restart_session.sh'
 FILE='~/.bashrc'
 grep -qF -- "$LINE" "$FILE" || echo "$LINE" >> "$FILE"
-read -n 1 -r -s -p $'Upload user-config.json and press enter to continue...\n'
+read -n 1 -r -s -p $'[Input Required] Upload user-config.json and press enter to continue...\n'
 #Need more elegant solution for handling exception here:
 [ -f ~/user-config.json ] && mv ~/user-config.json ~/amazon-redshift-infrastructure-automation/user-config.json
 cdk deploy --all --require-approval never
