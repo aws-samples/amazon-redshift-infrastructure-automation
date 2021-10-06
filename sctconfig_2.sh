acd0b816d056a4c948b2e4b57abcb58881078c28
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2021-10-06 10:03:41 -0400

    Update sctconfig_2.sh

--- sctconfig_2.sh
+++ sctconfig_2.sh
@@ -3,6 +3,5 @@ aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctcliauto.scts sct
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/sctrun.ps1 sctrun.ps1
 mkdir Drivers
 cd Drivers
-aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/mssql-jdbc-7.4.1.jre8.jar mssql-jdbc-7.4.1.jre8.jar
 aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/redshift-jdbc42-2.0.0.4.jar redshift-jdbc42-2.0.0.4.jar
 </script>
