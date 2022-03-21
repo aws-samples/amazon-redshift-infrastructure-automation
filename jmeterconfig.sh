46bb0bde1a675e331fc7452280ff488ee51b42b6
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2022-03-21 12:59:54 -0400

    Update jmeterconfig.sh

--- jmeterconfig.sh
+++ jmeterconfig.sh
@@ -4,8 +4,8 @@ echo %DATE% %TIME% >> %SystemRoot%\Temp\test.log
 cd c:\
 mkdir JMETER
 cd JMETER
-curl -o apache-jmeter-5.4.1.zip  https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.zip
-tar -xf apache-jmeter-5.4.1.zip
+curl -o apache-jmeter-5.4.3.zip  https://dlcdn.apache.org//jmeter/binaries/apache-jmeter-5.4.3.zip
+tar -xf apache-jmeter-5.4.3.zip
 curl -o AWSCLIV2.msi https://awscli.amazonaws.com/AWSCLIV2.msi
 msiexec.exe /i "AWSCLIV2.msi" /passive /l logcli.txt
 set PATH=c:\Program Files\Amazon\AWSCLIV2\;%PATH%
