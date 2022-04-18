0fae9a35dd164b704962bf23c6b8a664b234c1ca
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2022-04-18 13:27:24 -0400

    Update destroy.sh

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -15,6 +15,6 @@ then
         read -p $'[Input Required] Enter your on prem CIDR range (format xxx.xxx.xxx.xxx/xx): ' onprem_cidr
         export ONPREM_CIDR=$onprem_cidr
 fi
-cdk destroy --all --require-approval never
-python3 ./scripts/delete_secrets.py
 python3 ./scripts/delete_buckets.py
+cdk destroy --all --require-approval never
+#python3 ./scripts/delete_secrets.py
