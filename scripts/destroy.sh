e3feba415a775faaca66b44cbda8a2e5b3f7c6f1
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2022-05-03 20:29:25 -0400

    Update destroy.sh

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -39,5 +39,5 @@ secrets_list = [f"{stackname}-SourceDBPassword",
 sm_response = sm_client.list_secrets()
 for secret in sm_response['SecretList']:
     if secret['Name'] in secrets_list:
-      os.system('python amazon-redshift-infrastructure-automation/scripts/delete_secrets.py')
+      os.system('python /home/cloudshell-user/amazon-redshift-infrastructure-automation/scripts/delete_secrets.py')
 EOF
