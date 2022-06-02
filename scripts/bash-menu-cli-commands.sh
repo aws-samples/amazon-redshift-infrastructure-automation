c344271fd0776cb8cf0cda0399d11346cc311f1c
Shawn <shwnpat@amazon.com>
2022-06-01 20:25:08 -0500

    changed menu cli commands to match read array file names

--- scripts/bash-menu-cli-commands.sh
+++ scripts/bash-menu-cli-commands.sh
@@ -1,3 +1,3 @@
-aws ec2 --output text --query 'Vpcs[*].{VpcId:VpcId}' describe-vpcs > list.txt
+aws ec2 --output text --query 'Vpcs[*].{VpcId:VpcId}' describe-vpcs > vpclist.txt
 
-aws redshift --output text --query 'Clusters[*].{Endpoint:Endpoint.Address}' describe-clusters > test.txt
+aws redshift --output text --query 'Clusters[*].{Endpoint:Endpoint.Address}' describe-clusters > redshiftlist.txt
