704d3901969038055aaeb34697c7a190294332f4
Soniya Gaikwad <137340359+soniya-gaikwad@users.noreply.github.com>
2023-06-30 16:43:48 -0400

    Upgrade to CDKv2
    No errors with Redshift, Jmeter, and rest of deployment even with all n's. Few errors with VPC creation and datasharing. Will consult whether to upload other files or not until they're fixed; however, CDKv2 is fully implemented.

---
+++ redshift-stack-test.py
@@ -0,0 +1,260 @@
+from aws_cdk import aws_redshift
+from aws_cdk import aws_iam
+from aws_cdk import aws_secretsmanager
+# from aws_cdk import core
+import json
+import boto3
+from aws_cdk import aws_ec2
+from redshift_poc_automation.stacks.redshiftrole_stack import RSDefaultRole
+from redshift_poc_automation.stacks.redshiftload_stack import RedshiftLoadStack
+import builtins
+import getpass
+
+from aws_cdk import Stack
+from constructs import Construct
+
+# class RedshiftStack(core.Stack):
+class RedshiftStack(Stack):
+
+    def __init__(
+            self,
+            # scope: core.Construct, id: str,
+            scope: Construct, id: str,
+            vpc,
+            redshift_endpoint: str,
+            redshift_config: dict,
+            stack_log_level: str,
+            **kwargs
+    ) -> None:
+        super().__init__(scope, id, **kwargs)
+        stackname = id.split('-')[0]
+        redshift_client = boto3.client('redshift')
+        
+        if redshift_endpoint != "CREATE":
+            ec2_client = boto3.resource('ec2')
+            cluster_identifier = redshift_endpoint.split('.')[0]
+            self.redshift = redshift_client.describe_clusters(ClusterIdentifier=cluster_identifier)['Clusters'][0]
+            
+            self.password = stackname+'-RedshiftPassword'
+            
+            region_name = boto3.session.Session().region_name
+            session = boto3.session.Session()
+            client = session.client(
+                    service_name='secretsmanager',
+                    region_name=region_name,
+                )
+
+            try:
+                source_pwd = client.get_secret_value(
+                    SecretId=stackname+'-RedshiftPassword'
+                )['SecretString']
+            except Exception:
+                source_pwd = getpass.getpass(prompt='Redshift cluster password: ')
+                client.create_secret(
+                    Name=stackname+'-RedshiftPassword',
+                    SecretString=source_pwd,
+                    Description='Password of Redshift cluster'
+                )
+
+            redshift_sg_id = self.redshift['VpcSecurityGroups'][0]['VpcSecurityGroupId']
+            redshift_sg_name = ec2_client.SecurityGroup(redshift_sg_id).group_name
+
+            redshift_sg = aws_ec2.SecurityGroup.from_security_group_id(self, redshift_sg_name, redshift_sg_id)
+
+            dms_sg = vpc.get_vpc_security_group
+            redshift_sg.add_ingress_rule(peer=dms_sg, connection=aws_ec2.Port.all_traffic(), description="DMS input.")
+
+        else:
+
+            cluster_identifier = redshift_config.get('cluster_identifier')
+            database_name = redshift_config.get('database_name')
+            node_type = redshift_config.get('node_type')
+            number_of_nodes = int(redshift_config.get('number_of_nodes'))
+            master_user_name = redshift_config.get('master_user_name')
+            subnet_type = redshift_config.get('subnet_type')
+            encryption = redshift_config.get('encryption')
+            loadtpc = redshift_config.get('loadTPCdata')
+
+            # Create Cluster Password  ## MUST FIX EXCLUDE CHARACTERS FEATURE AS IT STILL INCLUDES SINGLE QUOTES SOMETIMES WHICH WILL FAIL
+            self.cluster_masteruser_secret = aws_secretsmanager.Secret(
+                self,
+                "RedshiftClusterSecret",
+                description="Redshift Cluster Secret",
+                secret_name=stackname+'-RedshiftClusterSecretAA',
+                generate_secret_string=aws_secretsmanager.SecretStringGenerator(
+                    exclude_punctuation=True, password_length=10),
+                # removal_policy=core.RemovalPolicy.DESTROY
+                removal_policy=RemovalPolicy.DESTROY
+            )
+
+            # IAM Role for Cluster
+            self.cluster_iam_role = aws_iam.Role(
+                self, "redshiftClusterRole",
+                assumed_by=aws_iam.ServicePrincipal(
+                    "redshift.amazonaws.com"),
+                managed_policies=[
+                    aws_iam.ManagedPolicy.from_aws_managed_policy_name(
+                        "AmazonS3ReadOnlyAccess"
+                    )
+                ]
+            )
+            self.cluster_masteruser_secret.grant_read(self.cluster_iam_role)
+            
+            publiclyaccessible = False
+            # Subnet Group for Cluster
+            if subnet_type == 'PUBLIC':
+                self.cluster_subnet_group = aws_redshift.CfnClusterSubnetGroup(
+                    self,
+                    "redshiftDemoClusterSubnetGroup",
+                    subnet_ids=vpc.get_vpc_public_subnet_ids,
+                    description="Redshift Demo Cluster Subnet Group"
+                )
+                publiclyaccessible = True
+            elif subnet_type == 'PRIVATE':
+                self.cluster_subnet_group = aws_redshift.CfnClusterSubnetGroup(
+                    self,
+                    "redshiftDemoClusterSubnetGroup",
+                    subnet_ids=vpc.get_vpc_private_subnet_ids,
+                    description="Redshift Demo Cluster Subnet Group"
+                )
+            elif subnet_type == 'ISOLATED':
+                self.cluster_subnet_group = aws_redshift.CfnClusterSubnetGroup(
+                    self,
+                    "redshiftDemoClusterSubnetGroup",
+                    subnet_ids=vpc.get_vpc_private_isolated_subnet_ids,
+                    description="Redshift Demo Cluster Subnet Group"
+                )
+
+            if number_of_nodes > 1:
+                clustertype = "multi-node"
+            else:
+                clustertype = "single-node"
+                number_of_nodes = None
+            
+            # Encryption boolean to True if Y or y
+            if encryption == "Y":
+                encryptcluster = bool(1)
+            elif encryption == "y":
+                encryptcluster = bool(1)
+            else:
+                encryptcluster = bool(0)
+
+            security_group_id = vpc.get_vpc_security_group_id
+
+            self.redshift = aws_redshift.CfnCluster(
+                self,
+                cluster_identifier,
+                db_name=database_name,
+                master_username=master_user_name,
+                cluster_type=clustertype,
+                master_user_password=self.cluster_masteruser_secret.secret_value.to_string(),
+                # master_user_password=master_password,
+                iam_roles=[self.cluster_iam_role.role_arn],
+                node_type=f"{node_type}",
+                encrypted=encryptcluster,
+                number_of_nodes=number_of_nodes,
+                publicly_accessible=publiclyaccessible,
+                cluster_subnet_group_name=self.cluster_subnet_group.ref,
+                vpc_security_group_ids=[security_group_id]
+            )
+
+        ###########################################
+        ################# OUTPUTS #################
+        ###########################################
+#         output_1 = core.CfnOutput(
+#             self,
+#             "RedshiftCluster",
+#             value=f"{self.demo_cluster.attr_endpoint_address}",
+#             description=f"RedshiftCluster Endpoint"
+#         )
+
+#         output_2 = core.CfnOutput(
+#             self,
+#             "RedshiftClusterPassword",
+#             value=(
+#                 f"https://console.aws.amazon.com/secretsmanager/home?region="
+#                 f"{core.Aws.REGION}"
+#                 f"#/secret?name="
+#                 f"{self.cluster_masteruser_secret.secret_arn}"
+#             ),
+#             description=f"Redshift Cluster Password in Secrets Manager"
+#         )
+#         output_3 = core.CfnOutput(
+#             self,
+#             "RedshiftIAMRole",
+#             value=(
+#                 f"{self.cluster_iam_role.role_arn}"
+#             ),
+#             description=f"Redshift Cluster IAM Role Arn"
+#         )
+
+        ############## FIX bug in CDK. Always returns None #########################
+
+        # output_4 = core.CfnOutput(
+        #     self,
+        #     "RedshiftClusterIdentifier",
+        #     value=(
+        #         f"{self.demo_cluster.cluster_identifier}"
+        #     ),
+        #     description=f"Redshift Cluster Identifier"
+        # )
+
+    # properties to share with other stacks
+    @property
+    def get_cluster(self):
+        if type(self.redshift) == dict:
+            return self.redshift
+        return self.redshift
+    
+    @property
+    def get_cluster_type(self):
+        return (type(self.redshift) == dict)
+
+    @property
+    def get_cluster_dbname(self) -> builtins.str:
+        if type(self.redshift) == dict:
+            return self.redshift['DBName']
+        return self.redshift.db_name
+
+    @property
+    def get_cluster_user(self) -> builtins.str:
+        if type(self.redshift) == dict:
+            return self.redshift['MasterUsername']
+        return self.redshift.master_username
+
+    @property
+    def get_cluster_password(self) -> builtins.str:
+        return self.redshift.master_user_password
+
+    @property
+    def get_cluster_host(self) -> builtins.str:
+        if type(self.redshift) == dict:
+            return self.redshift['Endpoint']['Address']
+        return self.redshift.attr_endpoint_address
+
+    @property
+    def get_cluster_iam_role(self) -> builtins.str:
+        if type(self.redshift) == dict:
+            return self.redshift['IamRoles'][0]['IamRoleArn']
+        return self.cluster_iam_role.role_arn
+
+    @property
+    def get_cluster_secret(self) -> builtins.str:
+        if type(self.redshift) == dict:
+            return self.password
+        return self.cluster_masteruser_secret.secret_name
+
+    ############## FIX bug in CDK. Always returns None #########################
+    @property
+    def get_cluster_endpoint(self) -> builtins.str:
+        return str(self.redshift.attr_endpoint_address)
+
+    @property
+    def get_cluster_identifier(self) -> builtins.str:
+        return str(self.redshift.attr_endpoint_address).split('.')[0]
+
+    @property
+    def get_cluster_availability_zone(self) -> builtins.str:
+        if type(self.redshift) == dict:
+            return self.redshift['AvailabilityZone']
+        return str(self.redshift.availability_zone)
---
+++ sct_stack.py
@@ -0,0 +1,122 @@
+from aws_cdk import aws_iam
+from aws_cdk import aws_ec2
+# from aws_cdk import core
+from aws_cdk import aws_secretsmanager
+import boto3
+import json
+
+from aws_cdk import Stack
+from constructs import Construct
+
+# class SctOnPremToRedshiftStack(core.Stack):
+class SctOnPremToRedshiftStack(Stack):
+
+    def __init__(
+            self,
+            # scope: core.Construct, id: str,
+            scope: Construct, id: str,
+            other_config: dict,
+            vpc,
+            stack_log_level: str,
+            onprem_cidr: str,
+            **kwargs
+
+    ) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        keyname = other_config.get('key_name')
+        secret_arn = 'RedshiftClusterSecretAA'
+        amiID = 'ami-042e0580ee1b9e2af'
+
+        account_id = boto3.client('sts').get_caller_identity().get('Account')
+
+        with open("./sctconfig.sh") as f:
+            user_data = f.read()
+
+        with open("./sctconfig_2.sh") as f_2:
+            user_data_2 = f_2.read()
+
+        # Instance Role and SSM Managed Policy
+
+        role_policy_document = {
+            "Version": "2012-10-17",
+            "Statement": [
+                {
+                    "Effect": "Allow",
+                    "Principal": {
+                        "AWS": "arn:aws:iam::" + account_id + ":root"
+                    },
+                    "Action": "sts:AssumeRole"
+                }
+            ]
+        }
+        client = boto3.client('iam')
+        roles = client.list_roles()
+        Role_list = roles['Roles']
+        for key in Role_list:
+            name = key['RoleName']
+            if name == 'window-cli-role':
+                windowcliexists = 1
+            else:
+                windowcliexists = 0
+
+        if windowcliexists == 1:
+          adminrole = aws_iam.Role(
+            self,
+            id='windows-cli-role',
+            assumed_by=aws_iam.ArnPrincipal("arn:aws:iam::" + account_id + ":root"),
+            role_name='windows-cli-role'
+          )
+          adminrole.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("AmazonS3ReadOnlyAccess"))
+
+        role = aws_iam.Role(self, "WindowsCLIrole", assumed_by=aws_iam.ServicePrincipal("ec2.amazonaws.com"))
+
+        role.add_to_policy(aws_iam.PolicyStatement(
+            actions=["sts:AssumeRole"],
+            resources=["arn:aws:iam::" + account_id + ":role/windows-cli-role"],
+            effect=aws_iam.Effect.ALLOW
+        ))
+        role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("service-role/AmazonEC2RoleforSSM"))
+        role.add_managed_policy(aws_iam.ManagedPolicy.from_aws_managed_policy_name("SecretsManagerReadWrite"))
+
+        # secrets_client = boto3.client(service_name='secretsmanager', region_name='us-east-1')
+        # get_secret_value_response = secrets_client.get_secret_value(
+        #    SecretId=secret_arn
+        # )
+        # redshift_pwd = [value for value in get_secret_value_response.values()][3]
+
+        ### TAKE THIS OUT SO THAT INSTANCE IS NOT PUBLIC ###
+        if aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType('PUBLIC')) != None:
+            subnet = aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType('PUBLIC'))
+        elif aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType('PRIVATE_WITH_NAT')) != None:
+            subnet = aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType('PRIVATE_WITH_NAT'))
+        else:
+            subnet = aws_ec2.SubnetSelection(subnet_type=aws_ec2.SubnetType('PRIVATE_ISOLATED'))
+
+        # my_security_group = aws_ec2.SecurityGroup(self, "SecurityGroup",
+        #                                       vpc=vpc.vpc,
+        #                                       description="Allow ssh access to ec2 instances",
+        #                                       allow_all_outbound=True
+        #                                       )
+        # my_security_group.add_ingress_rule(aws_ec2.Peer.ipv4('10.200.0.0/24'), aws_ec2.Port.tcp(22), "allow ssh access from the world")
+        # my_security_group.add_ingress_rule(my_security_group, aws_ec2.Port.all_tcp(), "self-referencing rule")
+        my_security_group = vpc.get_vpc_security_group
+
+        my_security_group.add_ingress_rule(peer=aws_ec2.Peer.ipv4(onprem_cidr), connection=aws_ec2.Port.tcp(3389),
+                                           description="RDP from anywhere")
+
+        custom_ami = aws_ec2.WindowsImage(aws_ec2.WindowsVersion.WINDOWS_SERVER_2019_ENGLISH_FULL_BASE);
+        # Instance
+        firstcommand = "\naws configure set role_arn arn:aws:iam::" + account_id + ":role/windows-cli-role\n"
+        input_data = user_data + firstcommand + user_data_2
+        instance = aws_ec2.Instance(self, "Instance",
+                                    instance_type=aws_ec2.InstanceType("m5.large"),
+                                    machine_image=custom_ami,
+                                    vpc=vpc.vpc,
+                                    vpc_subnets=subnet,
+                                    key_name=keyname,
+                                    role=role,
+                                    security_group=my_security_group,
+                                    #            resource_signal_timeout=core.Duration.minutes(5),
+                                    user_data=aws_ec2.UserData.custom(input_data)
+                                    )
