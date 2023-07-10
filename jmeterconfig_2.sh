b5b42ba4c9c46d4ca29608ae154ff403814cecc3
kaklisamir <81388407+kaklisamir@users.noreply.github.com>
2023-07-10 15:13:02 -0400

    Update jmeterconfig_2.sh
    updated jmeter to 5.6.1

--- jmeterconfig_2.sh
+++ jmeterconfig_2.sh
@@ -1,13 +1,13 @@
 aws configure set credential_source Ec2InstanceMetadata
-move "amazon-redshift-infrastructure-automation\scripts\Redshift Load Test.jmx" "C:\JMETER\apache-jmeter-5.4.3\Redshift Load Test.jmx"
-move amazon-redshift-infrastructure-automation\scripts\jmeter.bat "C:\JMETER\apache-jmeter-5.4.3\bin\jmeter.bat"
-move amazon-redshift-infrastructure-automation\scripts\redshift-jdbc42-2.0.0.4.jar "C:\JMETER\apache-jmeter-5.4.3\lib\redshift-jdbc42-2.0.0.4.jar"
+move "amazon-redshift-infrastructure-automation\scripts\Redshift Load Test.jmx" "C:\JMETER\Redshift Load Test.jmx"
+move amazon-redshift-infrastructure-automation\scripts\jmeter.bat "C:\JMETER\apache-jmeter-5.6.1\bin\jmeter.bat"
+move amazon-redshift-infrastructure-automation\scripts\redshift-jdbc42-2.0.0.4.jar "C:\JMETER\apache-jmeter-5.6.1\lib\redshift-jdbc42-2.0.0.4.jar"
 
-cd apache-jmeter-5.4.3
+cd apache-jmeter-5.6.1
 
 Rem aws s3 cp "s3://event-driven-app-with-lambda-redshift/scripts/Redshift Load Test.jmx" "Redshift Load Test.jmx"
-Rem aws s3 cp "s3://event-driven-app-with-lambda-redshift/scripts/jmeter.bat" "C:\JMETER\apache-jmeter-5.4.3\bin\jmeter.bat"
-mklink "C:\Users\Administrator\Desktop\jmeter.bat" "C:\JMETER\apache-jmeter-5.4.3\bin\jmeter.bat"
+Rem aws s3 cp "s3://event-driven-app-with-lambda-redshift/scripts/jmeter.bat" "C:\JMETER\apache-jmeter-5.6.1\bin\jmeter.bat"
+mklink "C:\Users\Administrator\Desktop\jmeter.bat" "C:\JMETER\apache-jmeter-5.6.1\bin\jmeter.bat"
 cd lib
 Rem aws s3 cp s3://event-driven-app-with-lambda-redshift/scripts/redshift-jdbc42-2.0.0.4.jar redshift-jdbc42-2.0.0.4.jar
 cd ext
