4e6d1621d637211115f4f85078ae531d3a2f426d
Soniya Gaikwad <137340359+soniya-gaikwad@users.noreply.github.com>
2023-07-19 14:48:57 -0400

    clean up comments

--- redshift_poc_automation/stacks/redshift_stack.py
+++ redshift_poc_automation/stacks/redshift_stack.py
@@ -1,7 +1,6 @@
 from aws_cdk import aws_redshift
 from aws_cdk import aws_iam
 from aws_cdk import aws_secretsmanager
-# from aws_cdk import core
 import json
 import boto3
 from aws_cdk import aws_ec2
@@ -14,12 +13,10 @@ from aws_cdk import Stack
 from constructs import Construct
 import aws_cdk 
 
-# class RedshiftStack(core.Stack):
 class RedshiftStack(Stack):
 
     def __init__(
             self,
-            # scope: core.Construct, id: str,
             scope: Construct, id: str,
             vpc,
             redshift_endpoint: str,
